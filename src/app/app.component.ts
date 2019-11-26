260f49e8ed1cb57612dccf4407f46c2899c6650c
Shubham Verma <shubhamv791@gmail.com>
2019-11-26 10:53:53 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { FormGroup,FormControl,NgForm } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit{
   name = 'Angular';
-  onSubmit(f) {
+  formG: FormGroup;
+  ngOnInit(){
+    this.formG = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl()
+    });
+    this.formG.get('firstName').statusChanges.subscribe
+    (value => console.log(value));
+  }
+  onSubmit(f:NgForm) {
     console.log(f.value);
     console.log(f.valid);
   } 
+  onSubmitReactive(){
+    this.formG.controls.firstName.setErrors({'error1':true});
+    this.formG.controls.firstName.setErrors({'error2':true});
+    console.log(this.formG.controls.firstName);
+    console.log(this.formG.controls.firstName.value+" "+this.formG.controls.lastName.value);
+  }
 }
 
